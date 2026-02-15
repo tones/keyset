423eedd763fab18a60a0bfae54ebffbbb4132c22
Tim Jones <timjones@Tims-MacBook-Pro.local>
2026-02-15 11:56:24 -0800

    Change octave shift icons from up/down arrows to left/right arrows

--- src/components/SortableKeySetList.tsx
+++ src/components/SortableKeySetList.tsx
@@ -120,8 +120,8 @@ function SortableKeySetCard({ keySet, songId, onDelete, onToggleNote, onShiftOct
                 title="Octave Down"
               >
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M12 5v14" />
-                  <path d="M19 12l-7 7-7-7" />
+                  <path d="M19 12H5" />
+                  <path d="M12 19l-7-7 7-7" />
                 </svg>
               </button>
               <button
@@ -130,8 +130,8 @@ function SortableKeySetCard({ keySet, songId, onDelete, onToggleNote, onShiftOct
                 title="Octave Up"
               >
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M12 19V5" />
-                  <path d="M5 12l7-7 7 7" />
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
                 </svg>
               </button>
             </>
